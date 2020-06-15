b2bc87d3675b2c939e35e797074c54b40c02b88c
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-15 04:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
-
+const { name, color, song, job, partner} = muppet
 const k_muppet = {
   k_name: 'Kermit',
   k_color: 'green',
